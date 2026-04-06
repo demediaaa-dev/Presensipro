606d68ec67cf0cf6e6d5efd7c52832c7ffff1fb3
demediaaa-dev <demediaaa@gmail.com>
2026-04-06 11:44:05 +0700

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbz-sqQbTA5-10_jndeHx_sToINGq1kipYsbnDXDM_JjDoPkhwoOW32XXCklyKK-Df3PHA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxAyCT8PcnXAP5RVOzeYu1OGi909-FfD6FaFqf3-rU77T5tX_oWK4CguOdRqQmiVhiuxw/exec";
 
 const API = {
   async call(data) { // Diubah dari 'post' ke 'call' agar sinkron dengan app.js
